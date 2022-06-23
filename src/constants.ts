93cd173258a7cddf49e456a908bf388b533bbae2
metiftikci <metiftikci@hotmail.com>
2022-06-23 10:11:08 +0300

    Specify full origin address

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { CardType, CardValue } from "./types";
 
 export const PORT = 3001;
-export const ORIGIN = "*";
+export const ORIGIN = "https://node-js-board-games.herokuapp.com/";
 export const CardTypes: CardType[] = ["Spade", "Club", "Hearth", "Diamond"];
 export const CardValues: CardValue[] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
