4f76ad4d6494dba8126138c4c611e180ada3fa4f
metiftikci <metiftikci@hotmail.com>
2022-06-23 10:06:27 +0300

    Allow cors

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { CardType, CardValue } from "./types";
 
 export const PORT = 3001;
-export const ORIGIN = "http://localhost:3000";
+export const ORIGIN = "*";
 export const CardTypes: CardType[] = ["Spade", "Club", "Hearth", "Diamond"];
 export const CardValues: CardValue[] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
